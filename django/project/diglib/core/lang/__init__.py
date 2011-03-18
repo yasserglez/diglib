0b5bd8cd25763fbcc254a4e7a05573160ed35428
Yasser González Fernández <ygonzalezfernandez@gmail.com>
2011-03-18 00:11:39 -0500

    Adding stopword lists to diglib.core.lang.

--- django/project/diglib/core/lang/__init__.py
+++ django/project/diglib/core/lang/__init__.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 
-# Based on the guess_language PyPi package by Kent Johnson
-# http://code.google.com/p/guess-language.
+# Language detection based on the guess_language PyPi package
+# by Kent Johnson (http://code.google.com/p/guess-language).
 
 import os
 import re
@@ -12,11 +12,34 @@ import bisect
 import codecs
 
 
+SUPPORTED_LANGS = ('en', 'es', 'it', 'fr', 'de')
+
+
 def get_lang(text):
     if isinstance(text, str):
         text = unicode(text, 'utf-8')
     text = _normalize(text)
-    return _check(text, MODELS.keys())
+    return _check(text, _MODELS.keys())
+
+
+def get_stopwords(lang):
+    return _STOPWORDS[lang]
+
+
+def _load_stopwords():
+    stopwords = {}
+    stopwords_dir = os.path.join(os.path.dirname(__file__), 'stopwords')
+    for stopword_file in os.listdir(stopwords_dir):
+        stopword_path = os.path.join(stopwords_dir, stopword_file)
+        if os.path.isfile(stopword_path):
+            with codecs.open(stopword_path, encoding='utf8', mode='r') as file:
+                stopword = []
+                for line in file:
+                    stopword.append(line.strip())
+            stopwords[stopword_file.lower()] = stopword
+    return stopwords
+
+_STOPWORDS = _load_stopwords()
 
 
 def _load_blocks():
@@ -39,12 +62,12 @@ def _load_blocks():
                 names.append(name)
     return endpoints, names
 
-BLOCK_ENDPOINTS, BLOCK_NAMES = _load_blocks()
+_BLOCK_ENDPOINTS, _BLOCK_NAMES = _load_blocks()
 
 def _unicode_block(c):
     # Returns the name of the Unicode block containing the character.
-    ix = bisect.bisect_left(BLOCK_ENDPOINTS, ord(c))
-    return BLOCK_NAMES[ix]
+    ix = bisect.bisect_left(_BLOCK_ENDPOINTS, ord(c))
+    return _BLOCK_NAMES[ix]
 
 
 def _make_nonalpha_re():
@@ -57,35 +80,34 @@ def _make_nonalpha_re():
     nonalpha = u''.join(nonalpha)
     return re.compile(nonalpha)
 
-NONALPHA_RE = _make_nonalpha_re()
-WHITESPACE_RE = re.compile('\s+', re.UNICODE)
-TRIGRAPH_RE = re.compile(r'(.{3})\s+(.*)')
+_NONALPHA_RE = _make_nonalpha_re()
+_WHITESPACE_RE = re.compile('\s+', re.UNICODE)
+_TRIGRAPH_RE = re.compile(r'(.{3})\s+(.*)')
 
 
 def _load_models():
     models = {}
     models_dir = os.path.join(os.path.dirname(__file__), 'trigraphs')
-    models_list = os.listdir(models_dir)
-    for model_file in models_list:
+    for model_file in os.listdir(models_dir):
         model_path = os.path.join(models_dir, model_file)
         if os.path.isfile(model_path):
             with codecs.open(model_path, encoding='utf8', mode='r') as file:
                 model = {}
                 for line in file:
-                    match = TRIGRAPH_RE.search(line)
+                    match = _TRIGRAPH_RE.search(line)
                     if match:
                         model[match.group(1)] = int(match.group(2))
             models[model_file.lower()] = model
     return models
 
-MODELS = _load_models()
+_MODELS = _load_models()
 
 
 def _normalize(text):
     # Convert to normalized Unicode, remove non-alpha and compress spaces.
     text = unicodedata.normalize('NFC', text)
-    text = NONALPHA_RE.sub(' ', text)
-    text = WHITESPACE_RE.sub(' ', text)
+    text = _NONALPHA_RE.sub(' ', text)
+    text = _WHITESPACE_RE.sub(' ', text)
     return text
 
 
@@ -136,8 +158,8 @@ def _check(text, langs):
     model = _ordered_model(text)
     for lang in langs:
         lower_lang = lang.lower()
-        if lower_lang in MODELS:
-            scores.append((_distance(model, MODELS[lower_lang]), lang))
+        if lower_lang in _MODELS:
+            scores.append((_distance(model, _MODELS[lower_lang]), lang))
     return min(scores)[1]
 
 
