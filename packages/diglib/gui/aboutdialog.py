2f0367385acfbbe006f7939bacff2135089bbf41
Yasser González Fernández <ygonzalezfernandez@gmail.com>
2013-04-03 11:42:56 -0400

    Add the year 2013 to the copyright notice.

--- packages/diglib/gui/aboutdialog.py
+++ packages/diglib/gui/aboutdialog.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 #
 # diglib: Digital Library
-# Copyright (C) 2011-2012 Yasser González Fernández <ygonzalezfernandez@gmail.com>
+# Copyright (C) 2011-2013 Yasser González Fernández <ygonzalezfernandez@gmail.com>
 #
 # This program is free software: you can redistribute it and/or modify it
 # under the terms of the GNU General Public License as published by the Free
